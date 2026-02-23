6376792bacc9a89032d02e77466432a7c7d2e670
Matt Tanguay-Carel <matthieutc@gmail.com>
2026-02-23 21:04:45 +0100

    Add animation on hover

--- src/edge.ts
+++ src/edge.ts
@@ -1,11 +1,14 @@
 import { GameObj } from "kaplay"
 import "kaplay/global"
 
-export const edgeFactory = (node1:GameObj, node2:GameObj, onRemove: (edge:GameObj) => void, getCurrentColor: Function): GameObj => {
+export const edgeFactory = (node1: GameObj, node2: GameObj, onRemove: (edge: GameObj) => void, getCurrentColor: Function): GameObj => {
   const dx = node2.pos.x - node1.pos.x
   const dy = node2.pos.y - node1.pos.y
   const distance = Math.sqrt(dx * dx + dy * dy)
   const angle = Math.atan2(dy, dx) * (180 / Math.PI)
+
+  const DEFAULT_COLOR = rgb(200, 200, 200)
+  const ANIM_DURATION = 0.2 // seconds
 
   const edge = add([
     rect(distance, 30),
@@ -13,19 +16,38 @@ export const edgeFactory = (node1:GameObj, node2:GameObj, onRemove: (edge:GameOb
     rotate(angle),
     anchor("left"),
     area(),
-    color([200, 200, 200]),
-    { node1, node2 },
+    color(DEFAULT_COLOR),
+    { node1, node2, currentTween: null },
     "edge",
   ])
 
   edge.onHover(() => {
     setCursor("pointer")
-    edge.color = Color.fromHex(getCurrentColor())
+    const targetColor = Color.fromHex(getCurrentColor())
+
+    if (edge.currentTween) edge.currentTween.cancel()
+
+    edge.currentTween = tween(
+      edge.color,
+      targetColor,
+      ANIM_DURATION,
+      (val) => edge.color = val,
+      easings.easeOutQuad
+    )
   })
 
   edge.onHoverEnd(() => {
     setCursor("default")
-    edge.color = rgb(200, 200, 200)
+
+    if (edge.currentTween) edge.currentTween.cancel()
+
+    edge.currentTween = tween(
+      edge.color,
+      DEFAULT_COLOR,
+      ANIM_DURATION,
+      (val) => edge.color = val,
+      easings.easeOutQuad
+    )
   })
 
   edge.onClick(() => {
